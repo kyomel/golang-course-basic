b86b37559dea9d0eff5c99eec9654d4efbcaa23a
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2022-10-30 23:18:51 +0700

    done learning at 30/10/2022

---
+++ exercise-2/ninja-6.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
+}
---
+++ exercise-2/ninja-8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const (
+	a     = 42
+	b int = 42
+)
+
+func main() {
+	fmt.Println(a, b)
+}
