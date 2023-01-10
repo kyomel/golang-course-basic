d4864743937ce3630cd496aa5e4c298c00d9411f
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-01-11 06:41:36 +0700

    done learning at 11/1/2023

---
+++ part-9/atomic.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+
+	var counter int64
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			runtime.Gosched()
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter))
+			// time.Sleep(time.Second)
+			wg.Done()
+		}()
+		fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
---
+++ part-9/raceCondition.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+
+	counter := 0
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
