b9a05a3d6ad063c271cc179bd6b8c435793f3c22
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-01-29 22:49:55 +0700

    done learning at 29/1/2023

---
+++ exercise-9/ninja-55.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incrementer := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			m.Lock()
+			v := incrementer
+			v++
+			incrementer = v
+			m.Unlock()
+			fmt.Println(incrementer)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("end value", incrementer)
+
+}
